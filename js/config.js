1347690dde4ea4827df110374b4db976b5bac6f5
kognavi <naokopanda8@gmail.com>
2025-03-31 23:16:59 +0900

    Add Cognito configuration

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-northeast-1_yPOJ7wvQ6',
+        userPoolClientId: '17sl1jn33f0qbb03je1jej7tlq',
+        region: 'ap-northeast-1'
+    },
+    api: {
+        invokeUrl: ''
+    }
+};
